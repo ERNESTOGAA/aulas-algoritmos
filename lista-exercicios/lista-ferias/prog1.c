d1d463dbb1865547b6eecd78509b39cccec4cbbb
Henrique Sousa <rickftime@gmail.com>
2015-01-05 14:26:29 -0200

    Changes to be committed: 	new file:   lista-exercicios/lista-ferias/lista-matrizes-ferias.pdf 	new file:   lista-exercicios/lista-ferias/prog1.c

---
+++ lista-exercicios/lista-ferias/prog1.c
@@ -0,0 +1,22 @@
+/*
+ *    1­Escreva   uma   função   que   armazene   em   uma   matriz   C(nxn)   o   resultado   da   multiplicação   de   outras  
+ *    duas matrizes A(nxn) e B(nxn).
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int ** multiplica( int **arr1, int **arr2 )
+{
+    int **total = malloc( sizeof( int *));
+    
+    total = malloc ( sizeof( int ) );
+
+    return total;
+}
+
+int main( int argc, char **argv )
+{
+
+
+    return 0;
+}
